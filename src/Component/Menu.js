54c17dfa7970912966e05d87ce08499bdb755d99
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-04 04:26:23 +0100

    React Destructuring functions

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,21 +1,28 @@
 import React from 'react'
 
 function Menu() {
-    function calculate(a,b) {
-        const add = a+b;
-        const subtract = a-b;
-        const multiply = a*b;
-        const divide = a/b;
-    return [add, subtract, multiply, divide];
-}
-const [add,subtract,multiply,divide] = calculate(1000000000,1000000000)
-    return (
-        <div>
-            <p> add : {add}</p>
-            <p> subtract : {subtract}</p>
-            <p> multiply : {multiply}</p>
-            <p> divide : {divide}</p>
-            
+        
+           const Vehicle= {
+               brand: 'Toyota',
+               Model: 'Prado',
+               type: 'SUV',
+               year: 2021,
+               color: 'red',
+               registration: {
+                   city: 'Oshodi',
+                   state: 'Lagos',
+                   country: 'Nigeria'
+           } 
+        }
+
+        vehicleOne(Vehicle)
+
+        function vehicleOne ({brand,Model,type,registration: { state }}) {
+            return <p> I have a {brand +' '+ Model +' '+ type}. I bought in {state} </p>
+        }
+        return (
+            <div>
+            {vehicleOne(Vehicle)}            
         </div>
     )
 }
