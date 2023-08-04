e71ae5082968ebd5f9dc72f4b24b2d35df565c6e
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-04 11:36:18 +0100

    Tenary condition

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,30 +1,33 @@
 import React from 'react'
 
 function Menu() {
-        
-           const Vehicle= {
-               brand: 'Toyota',
-               Model: 'Prado',
-               type: 'SUV',
-               year: 2021,
-               color: 'red',
-               registration: {
-                   city: 'Oshodi',
-                   state: 'Lagos',
-                   country: 'Nigeria'
-           } 
-        }
+    
+     return (
+     )
 
-        vehicleOne(Vehicle)
+    //        const Vehicle= {
+    //            brand: 'Toyota',
+    //            Model: 'Prado',
+    //            type: 'SUV',
+    //            year: 2021,
+    //            color: 'red',
+    //            registration: {
+    //                city: 'Oshodi',
+    //                state: 'Lagos',
+    //                country: 'Nigeria'
+    //        } 
+    //     }
 
-        function vehicleOne ({brand,Model,type,registration: { state }}) {
-            return <p> I have a {brand +' '+ Model +' '+ type}. I bought in {state} </p>
-        }
-        return (
-            <div>
-            {vehicleOne(Vehicle)}            
-        </div>
-    )
+    //     vehicleOne(Vehicle)
+
+    //     function vehicleOne ({brand,Model,type,registration: { state }}) {
+    //         return <p> I have a {brand +' '+ Model +' '+ type}. I bought in {state} </p>
+    //     }
+    //     return (
+    //         <div>
+    //         {vehicleOne(Vehicle)}            
+    //     </div>
+    // )
 }
 export default Menu
 
