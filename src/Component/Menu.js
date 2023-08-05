d6f14f29a2d0d429505ffea23b7359ac93407b67
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-05 22:18:46 +0100

    React lesson on Event handler

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,30 +1,12 @@
-import React, { Component } from "react";
+import React from 'react'
 
-class Menu extends Component {
-    constructor() {
-        super()
-        this.state = {
-            Brand: 'Ford',
-            Model: 'Mustang',
-            Color: 'red',
-            Year: 1964
-        }
+function Menu() {
+    const shoot = () => {
+        alert('this is an alert')
     }
-
-    changeColor =() => {
-        this.setState ({Color: 'blue'})
-    }
-    
-    render() {
-        return(
-            <>
-            <p>I have a {this.state.Brand + ' ' + this.state.Model}, it's a {this.state.Color} color. it was manufactured in the Year {this.state.Year}</p>
-            <button onClick={this.changeColor} >Change Color</button>
-            </>
-        
-            )
-    }
-
+    return (
+        <button onClick={shoot}>Give it a shoot</button>
+    )
 }
 
 export default Menu
