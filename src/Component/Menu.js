e296d1d7965f647413108d0495f32eeffdcbe91f
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-08 14:09:29 +0100

    React lesson on Hook and useState
     ;

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,27 +1,47 @@
-import React from 'react'
-
-function Car(props) {
-    return <li>I am a { props.brand }</li>
-}
+import React, {useState} from 'react'
 
 function Menu() {
-    const cars = [
-        {id: 1, brand: 'Toyota'},
-        {id: 2, brand: 'Audi'},
-        {id: 3, brand: 'Mercedi Benz'},
-        {id: 4, brand: 'Nissan'},
-        {id: 5, brand: 'Ferrari'},
-        {id: 6, brand: 'Cadallac'},
-        {id: 7, brand: 'Ford'},
-        {id: 7, brand: 'BMW'}
-    ]
-
+    const [colour, setColour] = useState('red');
     return (
         <>
-        {cars.map((car) => <Car key={car.id} brand = {car.brand} />)}
-        </>        
+            <h1>My favourite color is {colour}</h1>
+            <button
+            type='button'
+            onClick={() => {setColour('Blue')
+            }}
+            >Blue
+            </button>
+            
+            <button
+            type='button'
+            onClick={() => {setColour('Yellow')
+            }}
+            >Yellow
+            </button>
+
+            <button
+            type='button'
+            onClick={() => {setColour('Green')
+            }}
+            >Green
+            </button>
+
+            <button
+            type='button'
+            onClick={() => {setColour('white')
+            }}
+            >white
+            </button>
+
+            <button
+            type='button'
+            onClick={() => {setColour('Orange')
+            }}
+            >Orange
+            </button>
+
+        </>
     )
 }
 
 export default Menu
-
