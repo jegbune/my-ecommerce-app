91fe97d07e0f103e69c9a919f95771fe29ed408c
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-09 14:46:43 +0100

    React lesson on Form handling

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,46 +1,17 @@
 import React, {useState} from 'react'
 
 function Menu() {
-    const [colour, setColour] = useState('red');
+    const [name, setName] = useState('')
     return (
-        <>
-            <h1>My favourite color is {colour}</h1>
-            <button
-            type='button'
-            onClick={() => {setColour('Blue')
-            }}
-            >Blue
-            </button>
-            
-            <button
-            type='button'
-            onClick={() => {setColour('Yellow')
-            }}
-            >Yellow
-            </button>
-
-            <button
-            type='button'
-            onClick={() => {setColour('Green')
-            }}
-            >Green
-            </button>
-
-            <button
-            type='button'
-            onClick={() => {setColour('white')
-            }}
-            >white
-            </button>
-
-            <button
-            type='button'
-            onClick={() => {setColour('Orange')
-            }}
-            >Orange
-            </button>
-
-        </>
+        <form>
+            <label>Enter your name:
+                <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+        </form>
     )
 }
 
