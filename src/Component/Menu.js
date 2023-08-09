3ca204390320105b29c923a00bce36508ad181b2
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-09 17:30:02 +0100

    React lesson on Form target value

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,25 +1,26 @@
-import React, {useState} from 'react'
+import { useState } from "react"
+import React from 'react'
 
 function Menu() {
     const [name, setName] = useState('');
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+        // event.preventDefault();
         alert(`The name you entered was: ${name}`)
-        
+
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>Enter your name:
-                <input
-                type='text'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                />
+            <input 
+            type='text' 
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
+            />
             </label>
-            <input type='submit' />
         </form>
+        
     )
 }
 
