fee291cfc2052bb3d3cd19ec4d24346c403f76b2
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-10 13:19:50 +0100

    React lesson on CRUD app through form

--- src/Component/Crud.js
+++ src/Component/Crud.js
@@ -1,127 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-function MultiInputForm() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+function Crud() {
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
+    const [fName, setFName] = useState('')
+    const [lName, setLName] = useState('')
+    const [Email, setEmail] = useState('')
+    const [DOB, setDOB] = useState('')
+    const [Age, setAge] = useState('')
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
+    const handleFNameChange = (event) => {
+        setFName(event.target.value)
+    }
+    const handleLNameChange = (event) => {
+        setLName(event.target.value)
+    }
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+    const handleAgeChange = (event) => {
+        setAge(event.target.value)
+    }
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+    const handleDOBChange = (event) => {
+        setDOB(event.target.value)
+    }
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={firstName}
-        onChange={handleFirstNameChange}
-        placeholder="First Name"
-      />
-      <input
-        type="text"
-        value={lastName}
-        onChange={handleLastNameChange}
-        placeholder="Last Name"
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={handleEmailChange}
-        placeholder="Email"
-      />
-      <p>First Name: {firstName}</p>
-      <p>Last Name: {lastName}</p>
-      <p>Email: {email}</p>
-    </div>
-  );
+    const handleSubmit =(event) => {
+    //    event.preventDefault();
+        
+    }
+
+    return (
+        <form onSubmit={handleSubmit()}>
+            <label>Enter your First name:
+                <input
+                type='text'
+                name = 'FirstName'
+                value={fName}
+                onChange={handleFNameChange} 
+                />
+            </label>
+            
+            <label>Enter your Last name:
+                <input
+                type='text'
+                name = 'LastName'
+                value={lName}
+                onChange={handleLNameChange} 
+                />
+            </label>
+            <label>Enter your Email:
+                <input
+                type='text'
+                name = 'Email'
+                value={Email}
+                onChange={handleEmailChange} 
+                />
+            </label>
+            <label>Enter your Age:
+                <input
+                type='number'
+                name = 'Age'
+                value={Age}
+                onChange={handleAgeChange} 
+                />
+            </label>
+            <label>Enter your Date of Birth:
+                <input
+                type='Date'
+                name = 'DOB'
+                value={DOB}
+                onChange={handleDOBChange} 
+                />
+            </label>
+            <div>
+                <p>Your First Name: {fName}</p>
+                <p>Your Last Name: {lName}</p>
+                <p>Your Email: {Email}</p>
+                <p>Your Age{Age}</p>
+                <p>Your Date of birth:{DOB}</p>
+            </div>
+            
+        </form>
+    )
 }
 
-export default MultiInputForm;
-
-
-
-
-
-
-// import React, { useState } from 'react'
-
-// function Crud() {
-
-//     const [fName, setFName] = useState('')
-//     const [lName, setLName] = useState('')
-//     const [Email, setEmail] = useState('')
-//     const [Age, setAge] = useState('')
-
-//     const handleFNameChange = (event) => {
-//         setFName(event.target.value)
-//     }
-//     const handleLNameChange = (event) => {
-//         setLName(event.target.value)
-//     }
-//     const handleEmailChange = (event) => {
-//         setEmail(event.target.value)
-//     }
-//     const handleAgeChange = (event) => {
-//         setAge(event.target.value)
-//     }
-
-//     const handleSubmit =(event) => {
-//     //    event.preventDefault();
-        
-//     }
-
-//     return (
-//         <form onSubmit={handleSubmit()}>
-//             <label>Enter your First name:
-//                 <input
-//                 type='text'
-//                 name = 'FirstName'
-//                 value={fName}
-//                 onChange={handleFNameChange} 
-//                 />
-//             </label>
-            
-//             <label>Enter your Last name:
-//                 <input
-//                 type='text'
-//                 name = 'LastName'
-//                 value={lName}
-//                 onChange={handleLNameChange} 
-//                 />
-//             </label>
-//             <label>Enter your Email:
-//                 <input
-//                 type='text'
-//                 name = 'Email'
-//                 value={Email}
-//                 onChange={handleEmailChange} 
-//                 />
-//             </label>
-//             <label>Enter your Age:
-//                 <input
-//                 type='number'
-//                 name = 'Age'
-//                 value={Age}
-//                 onChange={handleAgeChange} 
-//                 />
-//             </label>
-//             <div>
-//                 <p>{fName}</p>
-//                 <p>{lName}</p>
-//                 <p>{Email}</p>
-//                 <p>{Age}</p>
-//             </div>
-            
-//         </form>
-//     )
-// }
-
-// export default Crud
+export default Crud
