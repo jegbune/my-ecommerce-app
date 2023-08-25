b35813c3aeba8286620f72bad11e28560b49c94f
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-25 23:16:42 +0100

    React lesson on Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import Menu from './Component/Menu';
-import Form from './Component/Form'
-import Crud from './Component/Crud';
+// import Menu from './Component/Menu';
+// import Form from './Component/Form'
+// import Crud from './Component/Crud';
 // import {Routes, Route} from 'react-router-dom'
 // import Home from './pages/Home'
 // import About from './pages/About'
 // import Contact from './pages/Contact'
-import Todos from './Component/Todos';
-import Styling from './Component/Styling.js'
-import State from './Component/State'
-import Timer from './Component/Timer';
-import UseEffect from './Component/useEffect';
-import Counter from './Component/Counter';
-import UseContext1 from './Component/UseContext1';
+// import Todos from './Component/Todos';
+// import Styling from './Component/Styling.js'
+// import State from './Component/State'
+// import Timer from './Component/Timer';
+// import UseEffect from './Component/useEffect';
+// import Counter from './Component/Counter';
+// import UseContext1 from './Component/UseContext1';
 
 function App() {
   return (
-    <UseContext1 />
+    
+    // <UseContext1 />
     // <div className='app'>
     //   {/* <Styling/> */}
     //   {/* <State /> */}
