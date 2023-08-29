9f7ef3bdce8662cc61e52ccb7f0f3dd5980ccf12
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-08-29 23:47:27 +0100

    React lesson on ecommerce nav

--- src/Component/ecommerceComponent/nav.js
+++ src/Component/ecommerceComponent/nav.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { FaTruckMoving } from 'react-icons/fa'  
-
+// import { FaTruckMoving } from 'react-icons/fa'  
+import './nav.css'
 function Nav() {
     return (
         <>
         <div className='free'>
                 <div className='icon'>
-                <FaTruckMoving/> 
+                {/* <FaTruckMoving/>  */}
                 </div>
-            <p>Free Shipping when shopping upto $1000</p>
+            <p>FREE Shipping when shopping upto $1000</p>
+        </div>
+        <div className='main_header'>
+            <div className='container'>
+                <div className='logo'>
+                    <img src=''></img>
+                </div>
+
+            </div>            
         </div>
         </>
     )
