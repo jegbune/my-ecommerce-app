4f1761f144d584a5ca8d87fd1b271a52399d4ab2
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-09-15 21:34:29 +0100

    React lesson on Nav Array for Ecommerce

--- src/Component/ecommerceComponent/nav.js
+++ src/Component/ecommerceComponent/nav.js
@@ -4,6 +4,7 @@ import { AiOutlineHeart } from 'react-icons/ai'
 import { BsBagCheck } from 'react-icons/bs'  
 import { AiOutlineUser } from 'react-icons/ai'  
 import { CiLogin } from 'react-icons/ci'  
+import { CiLogout } from 'react-icons/ci'  
 import './nav.css'
 import { Link } from 'react-router-dom'
 
@@ -47,22 +48,25 @@ function Nav() {
 
         <div className='header'>
             <div className='container'>
+                <div className='nav'>
                 <ul>
                     <li> 
-                        <Link to = '/'>Home</Link>
+                        <Link to = '/' className='link'>Home</Link>
                     </li>
                     <li> 
-                        <Link to = '/product'>Pruduct</Link>
+                        <Link to = '/product' className='link'>Product</Link>
                     </li>
                     <li> 
-                        <Link to = '/about'>About</Link>
+                        <Link to = '/about' className='link'>About</Link>
                     </li>
                     <li> 
-                        <Link to = '/contact'>Contact</Link>
+                        <Link to = '/contact' className='link'>Contact</Link>
                     </li>
                 </ul>
+                </div>
                 <div className='auth'>
                     <CiLogin />
+                    <CiLogout />
                 </div>
             </div>
 
