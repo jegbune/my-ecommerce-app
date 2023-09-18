ec0f063b289b741865af74d58a31140422d6c144
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-09-18 22:29:41 +0100

    React install auth0 for logging in and out

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Auth0Provider
+    domain="dev-8r17bfwspua1zyq2.us.auth0.com"
+    clientId="bu2wBb1Kpvm6VjzcRee1Rjduey7Iunbz"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <App />
-  </BrowserRouter>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
