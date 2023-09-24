de65503428859a061f3e903bee32a7f2eb70251a
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-09-24 23:54:17 +0100

    React Home page

--- src/Component/ecommerceComponent/Home.js
+++ src/Component/ecommerceComponent/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom'
 
 function Home() {
     return (
         <>
+        <div className='top_banner'>
+            <div className='container'>
+                <div className='detail'>
+                    <h2>The Best Note Book Collection 2023</h2>
+                    <BrowserRouter>
+                    <Link to= '/product'>Shop Now</Link>
+                    </BrowserRouter>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
