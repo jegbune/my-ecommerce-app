5e47353a48465537faa73765adc249a2150e51fb
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-10-01 07:25:54 +0100

    Ecommerce Homepage design

--- src/Component/ecommerceComponent/Home.js
+++ src/Component/ecommerceComponent/Home.js
@@ -9,10 +9,19 @@ function Home() {
             <div className='container'>
                 <div className='detail'>
                     <h2>The Best Note Book Collection 2023</h2>
-                    <Link to= '/product'>Shop Now <BsArrowRight/> </Link>
+                    <Link to= '/product' className='link'>Shop Now <BsArrowRight/> </Link>
                 </div>
                 <div className='img-box'>
                     <img src='./img/slider-img.png' alt='sliderimg' />
+                </div>
+            </div>
+        </div>
+        <div className='product_type'>
+            <div className='container'>
+                <div className='box'>
+                    <div className='img_box'>
+                        <img src='./img/Mobile Phone.png' alt='mobile' />
+                    </div>
                 </div>
             </div>
         </div>
