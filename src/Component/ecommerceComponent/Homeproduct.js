471d20abe176a54d71886aaca55958e55003e921
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-10-09 00:04:52 +0100

    Homepage development and styling

--- src/Component/ecommerceComponent/Homeproduct.js
+++ src/Component/ecommerceComponent/Homeproduct.js
@@ -9,30 +9,30 @@ const Homeproduct  =
     },
     {
         id: 2,
-        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
-        Cat: 'Tablet',
-        Price: '$723',
+        Title: 'Tracker with IP67 Waterproof Pedometer Smart watch.',
+        Cat: 'Smart Watch',
+        Price: '$168',
         Img: './img/tp2.jpg'
     },
     {
         id: 3,
-        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
-        Cat: 'Tablet',
-        Price: '$723',
+        Title: 'Cancelling Headphones Wireless.',
+        Cat: 'Headphone',
+        Price: '$49',
         Img: './img/tp3.jpg'
     },
     {
         id: 4,
-        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
-        Cat: 'Tablet',
-        Price: '$723',
+        Title: 'Profession Camera 4K Digital Video Camera.',
+        Cat: 'Camera',
+        Price: '$1049',
         Img: './img/tp4.jpg'
     },
     {
         id: 5,
-        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
-        Cat: 'Tablet',
-        Price: '$723',
+        Title: 'Mini Portable PD 22.5W Fast Charging Powerbank.',
+        Cat: 'Powerbank',
+        Price: '$49',
         Img: './img/tp5.jpg'
     },
     {
@@ -57,3 +57,4 @@ const Homeproduct  =
         Img: './img/tp8.jpg'
     }
 ]
+export default Homeproduct
