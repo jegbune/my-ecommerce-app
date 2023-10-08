81d949f35c7c526e2b687ae54fa268db00fbdc50
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-10-08 23:59:21 +0100

    Homepage development and styling

---
+++ src/Component/ecommerceComponent/Homeproduct.js
@@ -0,0 +1,59 @@
+const Homeproduct  =
+[
+    {
+        id: 1,
+        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
+        Cat: 'Tablet',
+        Price: '$723',
+        Img: './img/tp1.jpg'
+    },
+    {
+        id: 2,
+        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
+        Cat: 'Tablet',
+        Price: '$723',
+        Img: './img/tp2.jpg'
+    },
+    {
+        id: 3,
+        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
+        Cat: 'Tablet',
+        Price: '$723',
+        Img: './img/tp3.jpg'
+    },
+    {
+        id: 4,
+        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
+        Cat: 'Tablet',
+        Price: '$723',
+        Img: './img/tp4.jpg'
+    },
+    {
+        id: 5,
+        Title: 'Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.',
+        Cat: 'Tablet',
+        Price: '$723',
+        Img: './img/tp5.jpg'
+    },
+    {
+        id: 6,
+        Title: 'CPU Cooler 2 Heat Pipes 12mm 4 Pin PWM RGB for Intel.',
+        Cat: 'Heat pipe',
+        Price: '$156',
+        Img: './img/tp6.jpg'
+    },
+    {
+        id: 7,
+        Title: 'Playstation 4 2TB Slim Gaming Console.',
+        Cat: 'Gaming',
+        Price: '$2098',
+        Img: './img/tp7.jpg'
+    },
+    {
+        id: 8,
+        Title: 'Mini Portable Mobile Phone Powerbank for iphone.',
+        Cat: 'Powerbank',
+        Price: '$386',
+        Img: './img/tp8.jpg'
+    }
+]
