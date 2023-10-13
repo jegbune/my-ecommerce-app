bb4ec3be1c5b3cd6b35a1b08b2d81cf7ee8d1d8a
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-10-13 13:37:40 +0100

    Homepage Banner styling

--- src/Component/ecommerceComponent/Home.js
+++ src/Component/ecommerceComponent/Home.js
@@ -136,13 +136,15 @@ function Home() {
         </div>
                 
         <div className='banner'>
-            <div className='detail'>
-                <h4>LATEST TECHNOLOGY ADDED</h4>
-                <h3>Apple iPad 10.2 9th Gen - 2021</h3>
-                <p> <BsCurrencyDollar /> 986</p>
-            </div>
-            <div className='img-box'>
-                <img src='./img/slider-img.png' alt='sliderimg'/>
+            <div className='container'>
+                <div className='detail'>
+                    <h4>LATEST TECHNOLOGY ADDED</h4>
+                    <h3>Apple iPad 10.2 9th Gen - 2021</h3>
+                    <p><BsCurrencyDollar /> 986</p>
+                </div>
+                <div className='img-box'>
+                    <img src='./img/slider-img.png' alt='sliderimg'/>
+                </div>
             </div>
 
         </div>
