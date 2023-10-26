ff13c91dd6bd89ec1b50c85976777bdb8f8779a5
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-10-26 19:14:45 +0100

    Product page Development and styling

--- src/Component/ecommerceComponent/Product.js
+++ src/Component/ecommerceComponent/Product.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Productdetail from './Productdetail'
+import { AiOutlineHeart, AiOutlineShoppingCart } from 'react-icons/ai'
+import { BsEye } from 'react-icons/bs'
+import './product.css'
 
 function Product() {
+
     return (
         <>
-        <h3># Products</h3>
-        <p>Home . products</p>
         <div className='products'>
+        <h2># Products</h2>
+        <p>Home . products</p>
             <div className='container'>
                 <div className='filter'>
                     <div className='categories'> 
@@ -22,6 +26,30 @@ function Product() {
                 </div>
                 <div className='productbox'>
                     <div className='container'>
+                        {
+                            Productdetail.map((curElm)=> {
+                                return(
+                                    <>
+                                    <div className='box' key={curElm.id}>
+                                        <div className='img_box'>
+                                            <img src={curElm.Img} alt={curElm.Title}/>                                
+                                            <div className='icon'>
+                                                <li><AiOutlineShoppingCart /></li>
+                                                <li><BsEye /></li>
+                                                <li><AiOutlineHeart /></li>
+                                            </div>
+                                        </div>
+                                    <div className='detail'>
+                                    <p>{curElm.Cat}</p>
+                                    <h3>{curElm.Title}</h3>
+                                    <h4>{curElm.Price} </   h4>
+                                    </div>
+                                </div>
+                                    </>  
+                                )
+                            }) 
+                                 
+                            }
                         
                     </div>
                 </div>
